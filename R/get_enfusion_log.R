ec2481c915c507c1a3e5d483590b7b80b8002a01
twood886 <twood88@gmail.com>
2024-12-11 16:42:18 -0500

    fixing some typos

--- R/get_enfusion_log.R
+++ R/get_enfusion_log.R
@@ -9,17 +9,17 @@ get_enfusion_log <- function(enfusion_path, t = NULL) {
   # Set Enfusion Log Location
   path_f <- list.files(path = enfusion_path)
   log_loc <- file.path(
-    enfusion_path, 
+    enfusion_path,
     path_f[stringr::str_which(path_f, ".log")]
   )
   # Read Enfuison Log
   log_raw <-
     readr::read_delim(
-      log_loc, 
-      delim = ";", 
-      escape_double = FALSE, 
-      col_names = FALSE, 
-      trim_ws = TRUE, 
+      log_loc,
+      delim = ";",
+      escape_double = FALSE,
+      col_names = FALSE,
+      trim_ws = TRUE,
       skip = 1,
       show_col_types = FALSE
     )
@@ -35,12 +35,18 @@ get_enfusion_log <- function(enfusion_path, t = NULL) {
     log_tz_fmt
   )
   # Find where there are date entries
-  log_entries_loc <- stringr::str_which(log_raw$X1, paste0("^\\[", log_date_fmt, "\\]"))
+  log_entries_loc <- stringr::str_which(
+    log_raw$X1,
+    paste0("^\\[", log_date_fmt, "\\]")
+  )
   # Subset Log to just entries with date and times
-  log_entries <- log_raw[log_entries_loc,]
+  log_entries <- log_raw[log_entries_loc, ]
   # Get Time and Dates
   log_dates <- strptime(
-    stringr::str_extract_all(log_entries$X1, paste0(log_day_fmt, " ", log_time_fmt)),
+    stringr::str_extract_all(
+      log_entries$X1,
+      paste0(log_day_fmt, " ", log_time_fmt)
+    ),
     "%Y-%m-%d %H:%M:%S"
   )
 
@@ -60,10 +66,10 @@ check_enfusion_log_un <- function(enfusion_path, t) {
   log_entries <- get_enfusion_log(enfusion_path, t)
   any(
     stringr::str_detect(
-      log_entries, 
+      log_entries,
       paste0(
         "INFO  BrowserLoginComponent",
-        ".+", 
+        ".+",
         "Loaded URL:",
         ".+",
         "https://login-prod-us01.enfusionsystems.com/auth"
@@ -99,7 +105,7 @@ check_enfusion_log_load <- function(enfusion_path, t) {
   any(
     stringr::str_detect(
       log_entries,
-      paste0("INFO  ReportServiceImpl \\[PreloadWorker\\] Completed metadata preloads")
+      paste0("INFO  ReportServiceImpl \\[PreloadWorker\\] Completed metadata preloads") #nolint
     )
   )
 }
